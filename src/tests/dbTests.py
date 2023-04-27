7ae337e2cf7891a4d51206c8504d3596fccf3773
Leo Mark Castro <66856556+leomarkcastro@users.noreply.github.com>
2023-04-27 19:53:28 +0800

    first commit

---
+++ src/tests/dbTests.py
@@ -0,0 +1,17 @@
+import asyncio
+from prisma import Prisma
+
+
+async def main() -> None:
+    prisma = Prisma()
+    await prisma.connect()
+
+    # write your queries here
+    user = await prisma.user.create({
+        'name': 'Alice',
+    })
+
+    await prisma.disconnect()
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ src/tests/fileUploadRequest.py
@@ -0,0 +1,9 @@
+import requests
+
+url = 'http://127.0.0.1:8080/upload'
+files = {'ufile': open('file.txt', 'rb')}
+
+r = requests.post(url, files=files)
+
+print(r)
+print(r.text)
